8cd3d41be7aba6c2484c7b507e4beb96145bdf12
Ikhide Bright <ikhidebright@gmail.com>
2020-01-02 11:08:04 +0100

    added index, index.css, index.js files

--- index.js
+++ index.js
@@ -1,49 +1,55 @@
-// timer
+// get and set key tags
+let submitButton = document.querySelector("#sub");
+let checkForTwoSelection = [];
+let diffLevel = [];
+let diffCat = [];
+let error = [];
+let difficulty = document.querySelector("#difficulty");
+let category = document.querySelector("#category");
+
+
+//dynamic timer count down
 const timer = (minutes) => {
         setInterval(() => {
                 if (minutes === 0) {
                         // do nothing     
                 } else {
                         let timeUI = document.querySelector("#timeUI");
-                        timeUI.innerHTML = Number(--minutes) + " Minutes left";
+                        timeUI.innerHTML = Number(--minutes) + " minutes left";
                 }
-        }, 1000)
+        }, 60000)
 }
 
 
-window.onload = function () {
-let submitButton = document.querySelector("#sub");
-let btnArray = [];
-let diffLevel = [];
-let diffCat = [];
-
-// get select options value
-let difficulty = document.querySelector("#difficulty");
+// get selected difficulty option
 difficulty.addEventListener("change", function () {
         let diff = this.value;
-        btnArray.push(diff);
+        checkForTwoSelection.push(diff);
+        diffLevel = [];
         diffLevel.push(diff)
-        setInterval(btnUI(), 400)
+        setInterval(buttonUpdate(), 400)
 }, false);
 
-let category = document.querySelector("#category");
+
+// get selected category option
 category.addEventListener("change", function () {
-       let  cat = this.value;
-       btnArray.push(cat);
-       diffCat.push(cat);
-       setInterval(btnUI(), 400)
+        let cat = this.value;
+        checkForTwoSelection.push(cat);
+        diffCat = [];
+        diffCat.push(cat);
+        setInterval(buttonUpdate(), 400)
 }, false);
 
 
 // update button UI
-const btnUI = () => {
-        if(btnArray.length > 1) {
-        document.querySelector("#sub").disabled = false;
-        document.querySelector("#sub").className = 'btnact'
-} else {
-        document.querySelector("#sub").disabled = true;
-        document.querySelector("#sub").className = 'btndis'
-}
+const buttonUpdate = () => {
+        if (checkForTwoSelection.length > 1) {
+                document.querySelector("#sub").disabled = false;
+                document.querySelector("#sub").className = 'activeButton'
+        } else {
+                document.querySelector("#sub").disabled = true;
+                document.querySelector("#sub").className = 'disabledButton'
+        }
 }
 
 //show questions
@@ -56,64 +62,88 @@ const showQuestions = (results) => {
         doc.appendChild(headingQue);
 }
 
-//show loading button
-const loading = (shouldShowLoading) => {
-        if(shouldShowLoading) {
-        document.querySelector(".rules").textContent = 'Loading...';
-        document.querySelector(".rules").className = 'loading';
-} else {
-        document.querySelector(".rules").style.display = "none";
-        document.querySelector(".loading").style.display = "none";
-        document.querySelector(".rules").className = 'rules';
-}
-}
-
 
 // getting data from api
 const apiCall = (cat, diff) => {
-        loading(true);
         let api = `https://opentdb.com/api.php?amount=50&category=${cat}&difficulty=${diff}&type=multiple`
         fetch(api)
-        .then((res) => {
-               if(res.status = 200){
-                res.json().then((data) => showQuestions(data.results))
-        } else if (res.status > 200) {
-     console.log("an error occured");
-        }
-})
+                .then((res) => {
+                        if (res.status = 200) {
+                                res.json().then((data) => showQuestions(data.results))
+                        } else if (res.status > 200) {
+                                console.log("an error occured");
+                        }
+                }).catch((err) => {
+                        error.push(err.message);
+                })
 }
 
 
-// updateTime
+// update static Time and set count down minutes parameter
 const updateTime = () => {
-        // remove rules and Loading... 
-        loading(false);
-        if(diffLevel[0] === "easy") {
-        document.querySelector("#timeUI").innerHTML = 50 + " Minutes left";
-        } else if (diffLevel[0] === "medium") {
-                document.querySelector("#timeUI").innerHTML = 40 + " Minutes left";
-        } else if (diffLevel[0] === "hard") {
-                document.querySelector("#timeUI").innerHTML = 45 + " Minutes left";
+        if (diffLevel[0] === "easy" && !(error)) {
+                document.querySelector("#timeUI").innerHTML = 50 + " minutes left";
+                timer(50);
+        } else if (diffLevel[0] === "medium" && !(error)) {
+                document.querySelector("#timeUI").innerHTML = 40 + " minutes left";
+                timer(40);
+        } else if (diffLevel[0] === "hard" && !(error)) {
+                document.querySelector("#timeUI").innerHTML = 45 + " minutes left";
+                timer(45);
         }
+}
+
+// update the input areas
+const inputBox = () => {
+        if (diffLevel[0] === "easy" && !(error)) {
+                document.querySelector("#category").disabled = true;
+                document.querySelector("#difficulty").disabled = true;
+        } else if (diffLevel[0] === "medium" && !(error)) {
+                document.querySelector("#category").disabled = true;
+                document.querySelector("#difficulty").disabled = true;
+        } else if (diffLevel[0] === "hard" && !(error)) {
+                document.querySelector("#category").disabled = true;
+                document.querySelector("#difficulty").disabled = true;
+        }
+}
+
+
+//show loading button
+const loading = () => {
+        if (error) {
+                document.querySelector(".rules").style.display = 'none';
+                document.querySelector(".error").textContent = 'Sorry, an Error occurred. Check your Internet connection and try again';
+        } else {
+                document.querySelector(".error").style.display = "none";
+                document.querySelector(".rules").style.display = "none";
+        }
+}
+
+
+// if there is error
+const thereIsError = () => {
+        diffLevel = [];
+        diffCat = [];
+}
+
+
+// update the UI
+const UIcanInteract = () => {
+        inputBox();
+        updateTime();
+        loading();
+        thereIsError();
 }
 
 // submit button
 submitButton.addEventListener("click", () => {
-if(diffLevel[0] === "easy") {
-document.querySelector("#category").disabled = true;
-document.querySelector("#difficulty").disabled = true;
-timer(50);
-apiCall(diffCat[0], diffLevel[0]);
-} else if (diffLevel[0] === "medium") {
-        document.querySelector("#category").disabled = true;
-        document.querySelector("#difficulty").disabled = true;
-        timer(40);
-        apiCall(diffCat[0], diffLevel[0]);
-} else if (diffLevel[0] === "hard") {
-        document.querySelector("#category").disabled = true;
-        document.querySelector("#difficulty").disabled = true;
-        timer(45);
-        apiCall(diffCat[0], diffLevel[0]);
-}
+        //set  parameters for the api to use
+        if (diffLevel[0] === "easy") {
+                apiCall(diffCat[0], diffLevel[0]);
+        } else if (diffLevel[0] === "medium") {
+                apiCall(diffCat[0], diffLevel[0]);
+        } else if (diffLevel[0] === "hard") {
+                apiCall(diffCat[0], diffLevel[0]);
+        }
+        UIcanInteract();
 })
-}
